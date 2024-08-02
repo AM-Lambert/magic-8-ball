fadb8119664c6c3b24275d7eee23c872ba27615a
AM-Lambert <aaron.m.lambert@protonmail.com>
2024-08-02 10:49:53 -0600

    Create main.js

---
+++ main.js
@@ -0,0 +1,34 @@
+var userName = 'Aaron'
+let userGreeting = (userName) ? `Hello, ${userName}!` : 'Hello!'; {
+console.log(userGreeting)
+}
+var userQuestion = `Will ${userName} become a software engineer?`; {
+console.log(userQuestion)
+}
+var randomNumber = Math.floor(Math.random() * 8); {
+}
+var eightBall = randomNumber
+if (eightBall === 0) {
+    console.log ('It is certain')
+}
+else if (eightBall === 1) {
+    console.log('It is decidely so')
+}
+else if(eightBall === 2) {
+    console.log('Reply hazy, try again')
+}
+else if(eightBall === 3) {
+    console.log('Do not count on it')
+}
+else if(eightBall === 4) {
+    console.log('My sources say no')
+}
+else if(eightBall === 5) {
+    console.log('Outlook not so good')
+}
+else if(eightBall === 6) {
+    console.log('Signs point to yes')
+}
+else if(eightBall === 7) {
+    console.log('It is a virtual certainty')
+}
